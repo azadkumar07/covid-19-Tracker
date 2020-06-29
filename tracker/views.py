0a3ad959ddcedc9746261e542596f87d04966438
chandrakant <azadkumarlodhi07@gmail.com>
2020-06-30 01:08:29 +0530

    retry2

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,4 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-	return render(request,'front.html');
+	return HttpResponseRedirect("https://covid19bulandshahr.herokuapp.com/bsr/")
