4157cb8c077dd09ac163e63a3f3df68c87e58c00
chandrakant <azadkumarlodhi07@gmail.com>
2020-07-24 19:03:56 +0530

    first commit

--- tracker/settings.py
+++ tracker/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nob!--916qm%e45b%mfjqq$xh^nca_mgh*yw^pq*hydc_@uh_7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
 # Activate Django-Heroku.
 django_heroku.settings(locals())
